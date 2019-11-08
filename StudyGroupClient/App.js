865105c6165bd43c0ef60afd4b2787c185421ac0
wkywkywkywky <amberleywang@yahoo.com>
2019-11-07 20:53:15 -0800

    added logo to drawer

--- StudyGroupClient/App.js
+++ StudyGroupClient/App.js
@@ -1,15 +1,56 @@
 import {createAppContainer} from 'react-navigation';
-import {createDrawerNavigator} from 'react-navigation-drawer';
+import {createDrawerNavigator, DrawerItems} from 'react-navigation-drawer';
 import HomeScreen from './src/components/Home'
 import FindGroupScreen from './src/components/FindGroup'
 import MyGroupScreen from './src/components/MyGroup'
+import { Container, Content, Header, Body } from 'native-base'
+import React, {Component} from 'react'
+import {StyleSheet, Image} from 'react-native'
+
+const DrawerHeader = (props) => (
+  <Container>
+    <Header style={styles.drawerHeader}>
+      <Body>
+        <Image
+          style={styles.drawerImage}
+          source={require('./src/img/UCSD_logo.png')} />
+      </Body>
+    </Header>
+    <Content>
+      <DrawerItems {...props} />
+    </Content>
+
+  </Container>
+
+);
 
 const MyDrawerNavigator = createDrawerNavigator({
     Home: {screen: HomeScreen},
     FindGroup: {screen: FindGroupScreen},
     MyGroup: {screen: MyGroupScreen},
+  },{
+    contentComponent: DrawerHeader
   });
   
 const App = createAppContainer(MyDrawerNavigator);
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  drawerHeader: {
+    height: 200,
+    backgroundColor: 'white'
+  },
+  drawerImage: {
+    height: 150,
+    width: 150,
+    borderRadius: 75
+  }
+
+})
 
 export default App;
