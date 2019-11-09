f73b3a114b6ccb22dff4f07062f9570c37ba8ba8
Jun Suh <junsuh95@gmail.com>
2019-11-09 09:26:40 -0800

    Added search bar and edited FindGroup

--- StudyGroupClient/src/components/FindGroup.js
+++ StudyGroupClient/src/components/FindGroup.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import { Text, View, Image, Button} from 'react-native';
+import { Text, View, ScrollView, SafeAreaView, Image, Button} from 'react-native';
 import styles from '../styles/styles';
+import GroupCard from './shared/GroupCard';
 import MyHeader from './shared/MyHeader'
+import { Container, Content } from "native-base";
+import SearchBar from 'react-native-search-bar';
 
 class FindGroupScreen extends Component {
     static navigationOptions = {
@@ -14,32 +17,76 @@ class FindGroupScreen extends Component {
       ),
     };
   
-      state = {
-      group1_start_time: ''
-   }
-   // Use the URL for showing the groups according to this class Name.
-   componentDidMount = () => {
-      fetch('http://ec2-52-53-241-171.us-west-1.compute.amazonaws.com:3000/api/findGroupsWithClassName?className=CSE210', {
-         method: 'GET'
-      })
-      .then((response) => response.json())
-      .then((responseJson) => {
-         console.log(responseJson);
-         this.setState({
-            group1_start_time: responseJson[0].startTime
+    state = {
+      groups: [],
+      search: ''
+    }
 
-         })
-         console.log(this.state.group1_start_time);
-      })
-      .catch((error) => {
-         console.error(error);
+  formatContent(startTime, endTime, members) {
+    let content = "";
+    content += "Time: "+startTime+"-"+endTime;
+    for (let i=0; i< members.length; i++) {
+      content += "\n" + members[i].name||""
+    }
+    console.log(content)
+    return content;
+  }
+
+  refreshGroupCards() {
+    const cards = [];
+    for (let i=0; i < this.state.groups.length; i++) {
+        cards.push(
+          <GroupCard key={this.state.groups[i]._id} className={this.state.groups[i].class} content={this.formatContent(this.state.groups[i].startTime, this.state.groups[i].endTime, this.state.groups[i].members)}/>
+        ); 
+        // todo key
+    }
+    return cards;
+  }
+
+  updateSearch = search=> {
+    this.setState({ search });
+  };
+
+  // Use the URL for showing the groups according to the class name that was searched (e.g. submit 'CSE210' in search bar)
+  fetchData(search) {
+    const url = 'http://ec2-52-53-241-171.us-west-1.compute.amazonaws.com:3000/api/findGroupsWithClassName?className=';
+    fetch(url + search, {
+      method: 'GET'
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson);
+      this.setState({
+        groups: [...responseJson]
       });
-   }
-      render() {
+      this.refreshGroupCards();
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
+
+    render() {
+      const { search } = this.state;
+
       return (
-        <View>
-          <MyHeader title = 'Find Group' drawerOpen={() => this.props.navigation.openDrawer()}/>
-        <Text>Start Time = {this.state.group1_start_time}</Text>
+        <View style={{flex: 1}}>
+          <View>
+            <MyHeader title = 'Find Group' drawerOpen={() => this.props.navigation.openDrawer()}/>
+          </View>
+          <View style={{flex: 1}}>
+            <SearchBar ref="searchBar" placeholder="Search" onSearchButtonPress={(search) => {this.fetchData(search);}} value={search} />
+          </View>
+          <View style={{flex: 1, alignItems: "center"}}>
+            <ScrollView style={{width:"90%"}}>
+              <Container>
+                <Content padder>
+                  {this.refreshGroupCards()}
+                </Content>
+              </Container>
+            </ScrollView>
+          </View>
         </View>
       );
     }
