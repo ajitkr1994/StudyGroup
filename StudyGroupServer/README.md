4604e4dcc003c18ec2f1bbf7b52ceeec6b11304c
ikkane <ikkane@ucsd.edu>
2019-11-27 19:12:27 -0800

    Add db scripts

---
+++ StudyGroupServer/README.md
@@ -0,0 +1,19 @@
+Go to cloud.mongodb.com and create a free cluster, or install mongodb locally.
+Connect to cluster and remember your username and password.
+
+Here is an example connecting via the CLI to my cluster with my username admin.
+Switch the url to your cluster either locally or on the cloud.
+mongo "mongodb+srv://cluster0-xnpoq.mongodb.net/test"  --username admin 
+
+Now we will prepopulate the data. Replace admin and url with your username and url.
+mongo "mongodb+srv://cluster0-xnpoq.mongodb.net/test"  --username admin  ./StudyGroupServer/scripts/prepopulatedata.js
+
+To reinitialize the database in any case:
+mongo "mongodb+srv://cluster0-xnpoq.mongodb.net/test"  --username admin  ./StudyGroupServer/scripts/drop.js
+then
+mongo "mongodb+srv://cluster0-xnpoq.mongodb.net/test"  --username admin  ./StudyGroupServer/scripts/prepopulatedata.js
+
+Now to run the server:
+First export url mongo db URL:...
+npm install
+npm run serve
---
+++ StudyGroupServer/scripts/prepopulatedata.js
@@ -0,0 +1,55 @@
+var date1 = new Date("2019-11-26T14:12:00Z")
+var date2 = new Date("2019-11-26T16:12:00Z")
+var date3 = new Date("2019-11-26T18:12:00Z")
+var date4 = new Date("2019-11-26T20:12:00Z")
+
+var Alice = {
+    _id:1,
+    name  : "Alice",
+    email : "alice@ucsd.edu",
+    joinedGroups: []
+};
+
+var Bill = {
+    _id:2,
+    name  : "Bill",
+    email : "bill@ucsd.edu",
+    joinedGroups: []
+};
+
+var Carol = {
+    _id:3,
+    name  : "Carol",
+    email : "carol@ucsd.edu",
+    joinedGroups: []
+};
+
+var Group1 = {
+    _id:1,
+    class : "CSE210",
+    startTime: date1,
+    endTime: date2,
+    members: []
+}
+
+var Group2 = {
+    _id:2,
+    class : "CSE202",
+    startTime: date3,
+    endTime: date4,
+    members: []
+}
+
+Group1.members = [Alice._id, Bill._id];
+Group2.members = [Alice._id, Carol._id];
+
+Alice.joinedGroups = [Group1._id, Group2._id];
+Bill.joinedGroups = [Group1._id];
+Carol.joinedGroups = [Group2._id];
+
+db.users.insert(Alice);
+db.users.insert(Bill);
+db.users.insert(Carol);
+
+db.groups.insert(Group1);
+db.groups.insert(Group2);
