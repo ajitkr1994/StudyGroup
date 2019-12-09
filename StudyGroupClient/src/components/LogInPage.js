543487bb15615dd030fa44acf7382e646fdff661
Ajit <ajitkr1994@gmail.com>
2019-12-08 19:43:10 -0800

    Auth code

--- StudyGroupClient/src/components/LogInPage.js
+++ StudyGroupClient/src/components/LogInPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Button, StyleSheet } from 'react-native';
+import { View, Button, StyleSheet, Text} from 'react-native';
 import t from 'tcomb-form-native'; // 0.6.9
 //import DateTimePicker from '@react-native-community/datetimepicker';
 
@@ -18,9 +18,43 @@ class LogInPage extends Component {
       // do the things  
       const value = this._form.getValue(); // Send this value to backend.
       console.log('value: ', value);
-      this.setState({value: null}); // <-- Clear form after 'Create Group' has been clicked.
-      this.props.navigation.navigate('Main')
+      // this.setState({value: null}); // <-- Clear form after 'Create Group' has been clicked.
+
+      if (value) { // if validation fails, value will be null
+         let response = fetch("http://13.58.215.99:3000/api/login", {
+        method: "POST", 
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: value.emailAddress, 
+          password: value.password, 
+        })
+      });
+        console.log('Response', response.status);
+      // .then((response) => {
+      //   console.log('Response satus:', response.status);
+
+      //   if (response.status === 200)
+      //   {
+      //     this.props.navigation.navigate('Main')
+      //   }
+      //   else
+      //   {
+      //     // this.props.navigation.navigate('Main')
+      //         this.setState({value: null}); // <-- Clear form after 'Create Group' has been clicked.
+
+      //   }
+
+      // })
+      // .done();
     }
+
+    }
+
+
+
     handleRegister = () => {
       // do the things  
       const value = this._form.getValue(); // Send this value to backend.
@@ -40,6 +74,7 @@ class LogInPage extends Component {
           title="Log In"
           onPress={this.handleLogIn}
         />
+        <Text></Text>
         <Button
           title="Register"
           onPress={this.handleRegister}
--- StudyGroupClient/src/components/RegisterPage.js
+++ StudyGroupClient/src/components/RegisterPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Button, StyleSheet} from 'react-native';
+import { View, Button, StyleSheet, AsyncStorage} from 'react-native';
 import t from 'tcomb-form-native'; // 0.6.9
 //import DateTimePicker from '@react-native-community/datetimepicker';
 
@@ -13,13 +13,56 @@ const CourseInfo = t.struct({
     confirmPassword: t.String,
 });
 
+var STORAGE_KEY = 'id_token';
+
 class RegisterPage extends Component {
+
+    async _onValueChange(item, selectedValue) {
+    try {
+      await AsyncStorage.setItem(item, selectedValue);
+    } catch (error) {
+      console.log('AsyncStorage error: ' + error.message);
+    }
+  }
 
     handleSubmit = () => {
       // do the things  
       const value = this._form.getValue(); // Send this value to backend.
       console.log('value: ', value);
-      this.setState({value: null}); // <-- Clear form after 'Create Group' has been clicked.
+      // this.setState({value: null}); // <-- Clear form after 'Create Group' has been clicked.
+      if (value) { // if validation fails, value will be null
+
+    //   fetch("http://13.58.215.99:3000/api/user/signup", {
+    //     method: "POST", 
+    //     headers: {
+    //       'Accept': 'application/json',
+    //       'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //       username: value.username,
+    //       email: value.emailAddress, 
+    //       password: value.password, 
+    //     })
+    //   })
+    //   .then((response) => {
+    //     console.log('Response status:', response.status);
+
+    //     if (response.status === 200)
+    //     {
+    //       this.props.navigation.navigate('LogIn')
+    //     }
+    //     else
+    //     {
+    //       // this.props.navigation.navigate('Main')
+    //       this.setState({value: null}); // <-- If incorrect username/password is entered. Just delete the entered values.
+    //     }
+
+    //   })
+    //   .done();
+    }
+
+      
+
     }
   
     render() {
