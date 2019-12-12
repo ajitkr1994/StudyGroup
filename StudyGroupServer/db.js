ccdcab4a8963ac5eadc50cdaf5730f954256117f
EC2 Default User <ec2-user@ip-172-31-32-187.us-east-2.compute.internal>
2019-12-12 02:57:27 +0000

    patch

--- StudyGroupServer/db.js
+++ StudyGroupServer/db.js
@@ -2,6 +2,7 @@ const MongoClient = require('mongodb').MongoClient
 const mongo_url = process.env.DBURL + "?retryWrites=true&w=majority";
 console.log(mongo_url);
 
+var db;
 function getDB() {
     var dburl = process.env.DBURL + "?retryWrites=true&w=majority";
     if (process.env.TEST == 'true') {
@@ -11,12 +12,15 @@ function getDB() {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
+    if (!db) {
     return dbclient.connect().then(client => {
         if (process.env.TEST==true) {
             return db = client.db(global.__MONGO_DB_NAME__);
         }
-        return db = client.db();
+	db = client.db();
+        return db;
     });
+    } else { return db; }
 }
 
 module.exports = getDB;
